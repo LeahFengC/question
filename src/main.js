0ac3067fe9d5a011c2a6e3f634dc3d4396b6292e
可乐加冰 <846991641@qq.com>
2023-06-08 21:03:11 +0800

    [200~

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import {Cascader, Select, Option} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.config.productionTip = false
+
+Vue.use(Cascader)
+Vue.use(Select)
+Vue.use(Option)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
